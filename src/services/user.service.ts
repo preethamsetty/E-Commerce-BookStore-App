6ad0bcab3657f8d3be46f6ed59fdea1b8fb6d32d
LIKITH <likith.isekit2020@gmail.com>
2024-12-13 11:26:17 +0530

    [LIKITH] : Add : Add ResetPassword API

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,44 +1,17 @@
 import User from '../models/user.model';
-import { IUser } from '../interfaces/user.interface';
+import bcrypt from 'bcrypt';
 
 class UserService {
-
-  //get all users
-  public getAllUsers = async (): Promise<IUser[]> => {
-    const data = await User.find();
-    return data;
-  };
-
-  //create new user
-  public newUser = async (body: IUser): Promise<IUser> => {
-    const data = await User.create(body);
-    return data;
-  };
-
-  //update a user
-  public updateUser = async (_id: string, body: IUser): Promise<IUser> => {
-    const data = await User.findByIdAndUpdate(
-      {
-        _id
-      },
-      body,
-      {
-        new: true
-      }
+  //reset password
+  public resetPassword = async (body: {
+    email: string;
+    password: string;
+  }): Promise<void> => {
+    if (!body.email) throw new Error('Invalid Token');
+    await User.updateOne(
+      { email: body.email },
+      { $set: { password: await bcrypt.hash(body.password, 9) } }
     );
-    return data;
-  };
-
-  //delete a user
-  public deleteUser = async (_id: string): Promise<string> => {
-    await User.findByIdAndDelete(_id);
-    return '';
-  };
-
-  //get a single user
-  public getUser = async (_id: string): Promise<IUser> => {
-    const data = await User.findById(_id);
-    return data;
   };
 }
 
