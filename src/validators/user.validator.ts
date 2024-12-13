73e9c9fdfd80124d74dbe80ec386ef311b02cce2
prize15 <abhinavaseeja2000@gmail.com>
2024-12-13 16:07:13 +0530

    [ABHINAV] : Add : Create Validations for User

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,16 +1,64 @@
+
 import Joi from '@hapi/joi';
 import { Request, Response, NextFunction } from 'express';
-
+import HttpStatus from 'http-status-codes'
 class UserValidator {
-  public newUser = (req: Request, res: Response, next: NextFunction): void => {
+
+  // validate user registration data
+  public registerUser = (req: Request, res: Response, next: NextFunction): void => {
     const schema = Joi.object({
-      name: Joi.string().min(4).required()
+      firstname: Joi.string().required(),
+      lastname: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
     });
+
     const { error } = schema.validate(req.body);
-    if (error) {
-      next(error);
-    }
-    next();
+    if (error)
+      res.status(HttpStatus.BAD_REQUEST).json({Code:400, Error: error.message})
+    else
+      next();
+  };
+
+  // validate user login data
+  public loginUser = (req: Request, res: Response, next: NextFunction): void => {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error)
+      res.status(HttpStatus.BAD_REQUEST).json({Code:400, Error: error.message})
+    else
+      next();
+  };
+
+  // validate only email
+  public validateEmail = (req: Request, res: Response, next: NextFunction): void => {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error)
+      res.status(HttpStatus.BAD_REQUEST).json({Code:400, Error: error.message})
+    else
+      next();
+  };
+
+
+  // validate reset password
+  public resetPassword = (req: Request, res: Response, next: NextFunction): void => {
+    const schema = Joi.object({
+      password: Joi.string().min(6).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error)
+      res.status(HttpStatus.BAD_REQUEST).json({Code:400, Error: error.message})
+    else
+      next();
   };
 }
 
