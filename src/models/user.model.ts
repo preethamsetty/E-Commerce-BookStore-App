77e6bc328487f4cc308893488006b6590219755a
preethamsetty <preethambs2002@gmail.com>
2024-12-16 15:34:43 +0530

    [Preetham B S] refactor : Refactored register API for role-based registration

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -17,7 +17,12 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: true
+    },
+    role:{
+      type:String,
+      enum: ['user', 'admin'], 
     }
+
   },
   {
     timestamps: true
