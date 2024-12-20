551029298b800b01a8a2a5042d9841248d081d78
prize15 <abhinavaseeja2000@gmail.com>
2024-12-20 15:53:58 +0530

    [ABHINAV] : Added Pagination for Get Books

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -45,22 +45,24 @@ class BookController{
             }
       };
   
-  //Get All Books 
-    public getBooks = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-      const data = await this.BookService.getBooks();
-      try {
-        res.status(HttpStatus.OK).json({
-          code: HttpStatus.OK,
-          data,
-          message: 'Books fetched successfully'
-        });
+      //Get Books
+      public getBooks = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        const { page, limit } = req.query;
+          try {
+            const data = await this.BookService.getBooks(Number(page), Number(limit));
+            res.status(HttpStatus.OK).json({
+            code: HttpStatus.OK,
+            data,
+            message: 'Books fetched successfully'
+          });
       } catch (error) {
-        res.status(HttpStatus.BAD_REQUEST).json({
-            code: HttpStatus.BAD_REQUEST,
-            Error: error.message,
+          res.status(HttpStatus.BAD_REQUEST).json({
+          code: HttpStatus.BAD_REQUEST,
+          Error: error.message,
         });
-      }
-    };
+    }
+};
+
 
   //Upadate By Id
   public updateBookInfoById = async(req:Request, res:Response ,next:NextFunction):Promise<void> =>{
